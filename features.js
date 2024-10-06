442f17ca892f35e289f5a74a65f5dec5e78d0826
rhea <rhea@mordani.com>
2024-10-06 08:21:56 -0700

    updated 2nd page

--- features.js
+++ features.js
@@ -4,4 +4,29 @@ const urlParams = new URLSearchParams(window.location.search);
 // Retrieve the 'address' parameter from the URL
 const address = urlParams.get('address');
 
-console.log(address); // This will log the 'address' value to the console
+console.log(address); 
+
+
+import {
+    searchRegion,
+    RegionSearchValue,
+    SearchRegionRequestData,
+    SearchRegionResponse
+  } from "@googlemaps/region-lookup";
+
+  
+  const headers = {
+    "X-Goog-Api-Key": "AIzaSyDgJAElA3fsD-bZ-LXMVJpGd_4cbglLV2U",
+  };
+  
+  const data: SearchRegionRequestData = {
+    search_values: [
+      {
+        "address": address ,
+        "place_type": "address" as const,
+        "region_code": "us"
+      },
+    ],
+  };
+  const response = await regionLookupClient.searchRegion({ headers, data });
+  
